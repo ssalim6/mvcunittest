42b15e7ab7f33c6ec292e31f2614bc454d1af19b
ssalim <ssalim6@csc.com>
2019-11-04 23:37:30 +1100

    test

---
+++ Tests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using mvcunittest.Controllers;
+using mvcunittest.Models;
+using System.Web.Mvc;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Details_ReturnsDetailsView()
+        {
+            var controller = new HomeController();
+            var result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+
+        [Test]
+        public void Details_ReturnsLaptop()
+        {
+            var controller = new HomeController();
+            var result = controller.Details(2) as ViewResult;
+            var product = (Product)result.ViewData.Model;
+            Assert.AreEqual("Laptop", product.Name);
+
+        }
+    }
+}
---
+++ mvcunittest/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcunittest.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
